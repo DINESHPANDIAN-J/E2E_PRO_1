ea98f1559a9b91aa9987b69f6a74a4a160c55fbb
DINESHPANDIAN-J <datadinesh1@gmail.com>
2023-10-24 21:56:40 +0530

    Deloyement Configuration

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,7 +1,8 @@
 import sys
 import pandas as pd
-from src.exception import CustomException
-from src.utils import load_object
+sys.path.append('src')
+from exception import CustomException
+from utils import load_object
 
 
 class PredictPipeline:
